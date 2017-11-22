2ee941b1517765bbe58fa5f7c7638f5149fd6761
huigesi <791339970@qq.com>
2017-11-22 20:23:14 +0800

    测试BUG3

--- src/com/example/asynctaskdemo/ProgressBarActivity.java
+++ src/com/example/asynctaskdemo/ProgressBarActivity.java
@@ -7,6 +7,7 @@ import android.widget.ProgressBar;
 
 public class ProgressBarActivity extends Activity{
 	private ProgressBar pg;
+	AsyncTaskTest myTask;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +15,21 @@ public class ProgressBarActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.progressbar);
 		pg=(ProgressBar)findViewById(R.id.progressBar1);
-		new AsyncTaskTest().execute();
+		 myTask=new AsyncTaskTest();
+		myTask.execute();
 		
 	}
+	//反复按会等前一个AsyncTask记载完再继续，会出BUG
+	//可以通过与activity的生命周期同步的方法，在关闭时取消加载。
+	@Override
+	protected void onPause() {
+		super.onPause();
+		//cancel只是将AsyncTask状态标记为cancel，并不是取消行程执行
+		if(myTask!=null&&myTask.getStatus()==AsyncTask.Status.RUNNING){
+			myTask.cancel(true);
+		}
+	}
+
 	public class AsyncTaskTest extends AsyncTask<Void, Integer, Void>{
 		
 
@@ -29,14 +42,15 @@ public class ProgressBarActivity extends Activity{
 		@Override
 		protected Void doInBackground(Void... arg0) {
 			try {
-				Thread.sleep(4000);
 				for(int i=0;i<100;i++){
-
+					//用isCancelled方法判断是状态是否为cancel
+					if(isCancelled()){
+						break;
+					}
 					publishProgress(i);
 					Thread.sleep(300);
 				}
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			return null;
@@ -44,9 +58,12 @@ public class ProgressBarActivity extends Activity{
 
 		@Override
 		protected void onProgressUpdate(Integer... values) {
-			// TODO Auto-generated method stub
+			
+			if(isCancelled()){
+				return;
+			}
 			super.onProgressUpdate(values);
-			pg.setProgress(values[0]);
+			pg.setProgress(values[0]);//更新UI
 		}
 		
 		
